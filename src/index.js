e0951473d4327862c3abccd24de89b017667745c
Shannon Forsman <shannonforsman@Shannons-MacBook-Air.local>
2016-12-09 15:37:06 -0700

    added searchbar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import SearchBar from './components/search_bar';
 //const is an ES6 syntax - declaring a variable where it's the final value, we'll never reassign it (constant)
+
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 
