7b1b82ef291c699716687d19bbde642c115b0857
Shannon Forsman <sforsman1@gmail.com>
2016-12-11 11:37:38 -0700

    created basic react youtube app

---
+++ src/components/video_list_item.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const VideoListItem = ({video, onVideoSelect}) => {
+  //above, es6 says that there's a prop called video so please create a new variable called video and set it equal to the prop
+  const imageUrl = video.snippet.thumbnails.default.url;
+
+  return (
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl}/>
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
+
 import SearchBar from './components/search_bar';
-//const is an ES6 syntax - declaring a variable where it's the final value, we'll never reassign it (constant)
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+const API_KEY = '';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos : [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('climbing');
+  }
+
+  videoSearch(term) {
+    YTSearch({key : API_KEY, term: term}, (videos) => {
+      //with es6 if your key and value have the same name, you can shorten it from {videos: vidoes} to {videos}
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
 }
-
 
 ReactDom.render(<App />, document.querySelector('.container'));
